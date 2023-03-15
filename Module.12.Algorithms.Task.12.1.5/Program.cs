554c9b7ebc398d10c9b2a022e7f5ebc8776fb613
Dell <Dell@DESKTOP-IKPIQTL>
2023-03-15 20:58:44 +0300

    Добавьте файлы проекта.

---
+++ Module.12.Algorithms.Task.12.1.5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Module._12.Algorithms.Task._12._1._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
